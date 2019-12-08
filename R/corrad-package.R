098e46492d045ff28143994767bf1c1b041aab4b
taroyaoyama <taroyama.linda@gmail.com>
2019-12-08 21:40:16 +0900

    solve error resulted from rcppeigen src

---
+++ R/corrad-package.R
@@ -0,0 +1,7 @@
+
+## usethis namespace: start
+#' @useDynLib corrad, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+#' @import RcppEigen
+## usethis namespace: end
+NULL
--- src/corr_as.cpp
+++ src/corr_as.cpp
@@ -1,41 +1,32 @@
 
-//' Calculating correlation anomaly score
-//'   based on Kullback-Leibler divergence.
-//'
-//' @useDynLib corrad
-//' @importFrom Rcpp evalCpp
-//'
-//' @param SgA,SgB estimated (sparse) covariance matrix
-//' @param LmA,LmB estimated (sparse) precision matrix
-//' @export
-
-#include <iostream>
-#include <cmath>
-#include <Rcpp.h>
 #include <RcppEigen.h>
 
-using namespace std;
-using namespace Rcpp;
-using namespace Eigen;
-
-//[[Rcpp::depends("RcppEigen")]]
-typedef Map<MatrixXd> MapMatd;
+//' Multiply a number by two
+//'
+//' @param SgA,SgB covariance matrix of reference or test data.
+//' @param LmA,LmB inverse covariance matrix (i.e. precision matrix)
+//'   of reference or test data.
+//' @export
 
 //[[Rcpp::export]]
-VectorXd corr_as(MapMatd SgA, MapMatd SgB, MapMatd LmA, MapMatd LmB) {
+Eigen::VectorXd corr_as(
+        Eigen::Map<Eigen::MatrixXd> SgA,
+        Eigen::Map<Eigen::MatrixXd> SgB,
+        Eigen::Map<Eigen::MatrixXd> LmA,
+        Eigen::Map<Eigen::MatrixXd> LmB) {
 
     int M = SgA.rows();
-    VectorXd df = VectorXd::Zero(M);
+    Eigen::VectorXd df = Eigen::VectorXd::Zero(M);
 
     double lmA, lmB, sgA, sgB, dAB, dBA;
-    MatrixXd LA(M - 1, M - 1);
-    MatrixXd LB(M - 1, M - 1);
-    MatrixXd lA(M - 1,     1);
-    MatrixXd lB(M - 1,     1);
-    MatrixXd WA(M - 1, M - 1);
-    MatrixXd WB(M - 1, M - 1);
-    MatrixXd wA(M - 1,     1);
-    MatrixXd wB(M - 1,     1);
+    Eigen::MatrixXd LA(M - 1, M - 1);
+    Eigen::MatrixXd LB(M - 1, M - 1);
+    Eigen::MatrixXd lA(M - 1,     1);
+    Eigen::MatrixXd lB(M - 1,     1);
+    Eigen::MatrixXd WA(M - 1, M - 1);
+    Eigen::MatrixXd WB(M - 1, M - 1);
+    Eigen::MatrixXd wA(M - 1,     1);
+    Eigen::MatrixXd wB(M - 1,     1);
 
     for (int i = 0; i < M; i++) {
 
@@ -72,19 +63,19 @@ VectorXd corr_as(MapMatd SgA, MapMatd SgB, MapMatd LmA, MapMatd LmB) {
         wB.block(0, 0,     i,     1) = SgB.block(  0,   i,     i,     1);
         wB.block(i, 0, M-i-1,     1) = SgB.block(i+1,   i, M-i-1,     1);
 
-        MatrixXd tmp1(1, 1);
-        MatrixXd tmp2(1, 1);
+        Eigen::MatrixXd tmp1(1, 1);
+        Eigen::MatrixXd tmp2(1, 1);
         double tmp3;
 
         tmp1 = wA.transpose() * (lB - lA);
         tmp2 = 0.5 * ( (lB.transpose() * WA * lB).array() / lmB -
-                           (lA.transpose() * WA * lA).array() / lmA );
+            (lA.transpose() * WA * lA).array() / lmA );
         tmp3 = 0.5 * ( log(lmA / lmB) + sgA * (lmB - lmA) );
         dAB  = (tmp1.array() + tmp2.array() + tmp3)(0, 0);
 
         tmp1 = wB.transpose() * (lA - lB);
         tmp2 = 0.5 * ( (lA.transpose() * WB * lA).array() / lmA -
-                           (lB.transpose() * WB * lB).array() / lmB );
+            (lB.transpose() * WB * lB).array() / lmB );
         tmp3 = 0.5 * ( log(lmB / lmA) + sgB * (lmA - lmB) );
         dBA  = (tmp1.array() + tmp2.array() + tmp3)(0, 0);
 
